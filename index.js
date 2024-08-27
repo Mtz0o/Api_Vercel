42c375f784c0ed18d1ec1270ea393284c610f976
Mtz0o <matheuslimaaaaa15@gmail.com>
2024-08-27 09:56:37 -0300

    api vercel

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from express;
+
+import {montadoras, veiculos} from './dados.js'
+
+const app = express();
+
+app.get('/veiculo', (req, res) => {
+    res.json(veiculos);
+  });
+  
+  app.get('/montadoras', (req, res) => {
+    res.json(montadoras);
+  });
+
+  const PORT = 3030;
+
+  app.listen(PORT, ()=>{
+    console.log('Sistema Inicializado: ', 'Acesso: http://localhost/3030')
+
+  });
